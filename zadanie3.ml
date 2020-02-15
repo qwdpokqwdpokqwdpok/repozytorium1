a67a86956ca6584070207e67ee50d4cfc802ecd6
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-15 20:54:23 +0100

    Update zadanie3.ml

--- zadanie3.ml
+++ zadanie3.ml
@@ -1,4 +1,3 @@
-
 let slowa a =
   let wynik = ref 1 in
   let res1 = ref [|'a'|] in
@@ -7,14 +6,15 @@ let slowa a =
   let x = ref 0 in
   let y = ref 0 in
   let n = Array.length a in
-  while x < n do
-    if a.(!x) = !res1.(y) then (
-      if Array.length !res1 = !x + 1 then (
+  while !x < n do
+    if a.(!x) = !res1.(!y) then (
+      if ((Array.length !res1) = (!x + 1)) then (
         wynik := !wynik + 1;
-        res1 := Array.append !res1 !res2;
+        res1 := Array.append !res1copy !res2;
         res2 := !res1copy;
-        res1copy = !res1;)
-      y := !y + 1;)
-    x := !x + 1;
+        res1copy := !res1);
+        y := !y + 1);
+        x := !x + 1
   done;
-  !wynik;;
+  !wynik;
+  !res1;;
