15920332d84eee08e94ed1b9a40bacbbbab498fa
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-16 16:01:58 +0100

    Update Egz1516_2.ml

--- Egz1516_2.ml
+++ Egz1516_2.ml
@@ -1,1 +1,13 @@
-
+let autobusy p a =
+  let wynik = ref 0 in
+  let n = Array.length p in
+  let m = Array.length a in
+  let suma_ludzi = ref 0 in
+  try
+    for i = a.(m - 1) to 
+  for i = m - 1 downto 1 do
+    for j = a.(i) downto a.(i - 1) + 1 do
+      suma_ludzi := !suma_ludzi + p.(j)
+    done;
+    
+    
