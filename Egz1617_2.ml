e55ee1237b66d3823415b9cfe23496c218006e69
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-17 00:01:45 +0100

    Update Egz1617_2.ml

--- Egz1617_2.ml
+++ Egz1617_2.ml
@@ -1,1 +1,23 @@
-let pora 
+ type pora = Wiosna | Lato | Jesien | Zima;;
+ let pora arr =
+  let nastepna p = function
+    | Wiosna -> Lato
+    | Lato -> Jesien
+    | Jesien -> Zima
+    | Zima -> Wiosna in
+  let n = Array.length arr in
+  let wynik = ref (- 1) in
+  for i = 0 to n - 1 do
+    let licznik = ref 3 in
+    let indeks = ref (i + 1) in
+    let aktualna_pora = ref arr.(i) in
+    while !licznik > 0 && !indeks < n do
+      if arr.(!indeks) = nastepna !aktualna_pora then (
+        licznik := !licznik - 1;
+        aktualna_pora := nastepna !aktualna_pora;
+        if !licznik = 0 then wynik := max 0 (min !wynik (!indeks + 1 - i)););
+      indeks := !indeks + 1;
+    done;
+  done;
+  !wynik;;
+  
