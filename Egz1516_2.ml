bac9db933c7fa4676defa27e85e5c0e497582437
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-16 18:14:08 +0100

    Update Egz1516_2.ml

--- Egz1516_2.ml
+++ Egz1516_2.ml
@@ -1,13 +1,23 @@
+exception Nic_nie_jezdzi_a_sa_ludzie;;
+exception Ludzie_przyszli_za_pozno;;
 let autobusy p a =
   let wynik = ref 0 in
   let n = Array.length p in
   let m = Array.length a in
   let suma_ludzi = ref 0 in
+  if n = 0 then 0 else
+  if m = 0 then
   try
-    for i = a.(m - 1) to n - 1 do
-      if p.(i) > 0 then raise Exit
+    for i = 0 to n - 1 do
+      if p.(i) > 0 then raise Nic_nie_jezdzi_a_sa_ludzie
     done;
-  with Exit -> - 1
+    0;
+  with Nic_nie_jezdzi_a_sa_ludzie -> - 1
+  else
+  try
+    for i = a.(m - 1) + 1 to n - 1 do
+      if p.(i) > 0 then raise Ludzie_przyszli_za_pozno
+    done;
   for i = m - 1 downto 1 do
     for j = a.(i) downto a.(i - 1) + 1 do
       suma_ludzi := !suma_ludzi + p.(j)
@@ -19,7 +29,8 @@ let autobusy p a =
   for i = a.(0) downto 0 do
     suma_ludzi := !suma_ludzi + p.(i)
   done;
-  let_wynik_dla_czasu_a0 =
+  let wynik_dla_czasu_a0 =
     (!suma_ludzi / m) + (if !suma_ludzi mod m = 0 then 0 else 1) in
-  if wynik_dla_czasu_a0 > !wynik then wynik := wynik dla czasu_a0;
-  !wynik
+  if wynik_dla_czasu_a0 > !wynik then wynik := wynik_dla_czasu_a0;
+  !wynik;
+  with Ludzie_przyszli_za_pozno -> - 1;;
