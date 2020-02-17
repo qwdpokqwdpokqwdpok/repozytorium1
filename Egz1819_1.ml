479406ea4945a93c8b6da0170fb4f14ff8822df5
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-17 15:19:17 +0100

    Update Egz1819_1.ml

--- Egz1819_1.ml
+++ Egz1819_1.ml
@@ -1,1 +1,15 @@
-
+let transpose lista =
+  let odwrocone =
+    (List.fold_left
+      (fun acc l ->
+        let doklejanie =
+          (List.fold_left
+            (fun ((hd1::tl1), (hd2::tl2)) x -> (((x::hd2)::hd1::tl1), tl2))
+            ([[]], acc)
+            l) in
+        match doklejanie with (l, _) ->
+          List.tl (List.rev l))
+      []
+      lista) in
+  List.map (List.rev) odwrocone;;
+       
