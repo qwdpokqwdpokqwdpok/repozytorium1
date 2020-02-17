ee4e06ef3c8e89e8ec398f244e2f52a76a48b0ab
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-17 12:09:41 +0100

    Update Egz1718_2.ml

--- Egz1718_2.ml
+++ Egz1718_2.ml
@@ -1,1 +1,7 @@
-
+(* drzewa zrownowazone -> roznica wysokosci poddrzew ograniczona ->
+-> stosunek ilosci wezlow w poddrzewach ograniczony *)
+(* binary, roznica wysokosci <= 1 ->
+-> max wezlow dla wysokosci n -> 2^n-1
+-> min wezlow dla wysokosci n -> 
+  r(0)=0; r(1)=1; r(n)=r(n-1)+r(n-2)+1
+  0 1 2 4 7 12 20 33 54
