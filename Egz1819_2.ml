b24d4be506ac2f27deef81d62508246e8719f9d5
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-17 10:08:58 +0100

    Update Egz1819_2.ml

--- Egz1819_2.ml
+++ Egz1819_2.ml
@@ -1,1 +1,12 @@
+type 'a heap = Node of 'a heap * 'a * 'a heap | Null;;
 
+let rekonsrukcja arr =
+  let dodaj_korzen h k =
+    Node of h * k * Null in
+  let dodaj_prawe_poddrzewo (Node of h1 * k * Null) h2 =
+    Node of h1 * k * h2 in
+  let nowe_poddrzewo k =
+    Node of Null * h * Null in
+  let wynik =
+    Array.fold_left
+      (fun (
