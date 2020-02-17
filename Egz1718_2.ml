3ebf55ffab31c3e93a4d9e9e2e5f8ec37c3bb8d3
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-17 18:55:07 +0100

    Update Egz1718_2.ml

--- Egz1718_2.ml
+++ Egz1718_2.ml
@@ -6,3 +6,13 @@
   r(0)=0; r(1)=1; r(n)=r(n-1)+r(n-2)+1
   0 1 2 4 7 12 20 33 54
 *)
+(* idziesz w lewo i patrzysz na relacje elementu w ktorym stoisz i poprzedniego
+jest ..., wiekszy, mniejszy -> poprzedni byl takim punktem,
+ze wszystko na lewo bylo od niego mniejsze
+idziesz dalej i po ilosci ruchow ograniczonej przez
+max mozliwa roznice wysokosci natrafiasz na ..., wiekszy, mniejszy
+znow cofasz sie o 1
+te kilka punktow po drodze moglo byc mniejszych od tego, w ktorym teraz stoisz
+reszta na prawo jest wieksza
+szukasz w prawej albo lewej polowie
+ale co jezeli cos jest mniejsze od 1 i wieksze od 2 punktu
