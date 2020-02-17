3f5c26a9cbec7aa1b7f200e70bd63078a3b5669f
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-17 02:54:32 +0100

    Update Egz1718_1.ml

--- Egz1718_1.ml
+++ Egz1718_1.ml
@@ -1,1 +1,38 @@
+type symbol =
+| Liczba of int
+| Plus | Minus | Razy | Podziel
+| Nawias_Otw | Nawias_Zam;;
+
+let kalkulator lista =
+  let wykonaj a (y, 0, 0, 0, 0) =
+    match a with
+    | (0, 0, 0, 0, 0) -> (y, 0, 0, 0, 0)
+    | (x, 1, 0, 0, 0) -> (x + y, 0, 0, 0, 0)
+    | (x, 0, 1, 0, 0) -> (x - y, 0, 0, 0, 0)
+    | (x, 0, 0, 1, 0) -> (x * y, 0, 0, 0, 0)
+    | (x, 0, 0, 0, 1) -> (x / y, 0, 0, 0, 0) in
+  let plus = function
+    | (x, 0, 0, 0, 0) -> (x, 1, 0, 0, 0) in
+  let minus = function
+    | (x, 0, 0, 0, 0) -> (x, 0, 1, 0, 0) in
+  let razy = function
+    | (x, 0, 0, 0, 0) -> (x, 0, 0, 1, 0) in
+  let podz = function
+    | (x, 0, 0, 0, 0) -> (x, 0, 0, 0, 1) in
+  let otw = function
+    | l -> (0,0,0,0,0)::l in
+  let zam = function
+    | a::b::c -> (b wykonaj a)::c
+  List.fold_left
+    (fun (a::b) s -> match s with
+      | Liczba x -> (wykonaj x a)::b
+      | Plus -> (plus a)::b
+      | Minus -> (mius a)::b
+      | Razy -> (razy a)::b
+      | Podzielic -> (podz a)::b
+      | Nawias_Otw -> otw a::b
+      | Nawias_Zam -> zam a::b)
+    [(0,0,0,0,0)]
+    lista;;
+  
 
