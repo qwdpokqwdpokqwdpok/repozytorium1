31fb54255444789a2a6110eabc5880f858e7ab9c
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-02-17 15:59:49 +0100

    Update Egz1819_1.ml

--- Egz1819_1.ml
+++ Egz1819_1.ml
@@ -1,14 +1,20 @@
 let transpose lista =
+  let jadro (l1, l2) x =
+    match (l1, l2) with
+    | ([], []) -> ([[x]], [])
+    | ([], (hd2::tl2)) -> ([(x::hd2)], tl2)
+    | ((hd1::tl1), []) -> (([x]::hd1::tl1), [])
+    | ((hd1::tl1), (hd2::tl2)) -> (((x::hd2)::hd1::tl1), tl2) in
   let odwrocone =
     (List.fold_left
       (fun acc l ->
         let doklejanie =
           (List.fold_left
-            (fun ((hd1::tl1), (hd2::tl2)) x -> (((x::hd2)::hd1::tl1), tl2))
-            ([[]], acc)
+            (jadro)
+            ([], acc)
             l) in
         match doklejanie with (l, _) ->
-          List.tl (List.rev l))
+          List.rev l)
       []
       lista) in
   List.map (List.rev) odwrocone;;
